6d8f28a64c3ed9714e28042f00882a8794c68d5e
Sam Clearman <sam@scaleapi.com>
2018-07-09 15:21:38 -0700

    Things

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,10 +15,14 @@
 
 ;; Set up appearance
 ;; Solarized theme
-(use-package solarized-theme
+;; (use-package solarized-theme
+;;   :config
+;;   (setq solarized-high-contrast-mode-line t)
+;;   (load-theme 'solarized-dark t))
+(use-package material-theme
+  :ensure
   :config
-  (setq solarized-high-contrast-mode-line t)
-  (load-theme 'solarized-dark t))
+    (load-theme 'material t))
 ;; Get rid of chrome
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -29,6 +33,9 @@
 
 ;; Just quit (currently os x only)
 (load (concat user-emacs-directory "scripts/quit"))
+
+;; Revert all buffers function
+(load (concat user-emacs-directory "scripts/revert-all"))
 
 ;; Fucking autosave
 (setq backup-directory-alist
@@ -52,12 +59,19 @@
   :ensure
   :config
     (setq js-indent-level 2)
-    (setq-default js2-strict-trailing-comma-warning nil))
+    (setq-default js2-strict-trailing-comma-warning nil)
+    (add-to-list 'auto-mode-alist '("\\.jsm?\\'" . js2-mode)))
 
 (use-package rjsx-mode
   :ensure
   :config
+    (add-to-list 'auto-mode-alist '("frontend\\/.*\\.js\\'" . rjsx-mode)) ;; this is just for Scale
     (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode)))
+
+(use-package typescript-mode
+  :ensure
+  :config
+    (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode)))
 
 ;; Load packages / elisp files
 
@@ -68,7 +82,8 @@
 ;; Magit
 (use-package magit
   :bind (("C-x g" . magit-status)
-	 ("C-x M-g" . magit-dispatch-popup)))
+	 ("C-x M-g" . magit-dispatch-popup))
+  :config (setq-default magit-list-refs-sortby "-committerdate"))
 
 ;; Smooth scrolling
 (use-package sublimity
@@ -97,22 +112,12 @@
   :bind (("C-," . avy-goto-word-1)
 	 ("C-." . avy-goto-char)))
 
-;; Ido
-;; (use-package ido-vertical-mode
-;;   :config
-;;   (setq ido-enable-flex-matching t)
-;;   (setq ido-everywhere t)
-;;   (ido-mode 1)
-;;   (ido-vertical-mode 1)
-;;   (setq ido-vertical-define-keys 'C-n-and-C-p-only))
-
 ;; Ivy is a completion engine
 (use-package ivy
   :config
   (ivy-mode 1)
   (setq ivy-use-virtual-buffers t
 	ivy-count-format "%d/%d "))
-
 
 ;; Swiper provides a more fully featured isearch interface.
 (use-package swiper
@@ -124,7 +129,9 @@
   :ensure
   :bind (("C-c g" . counsel-git)
 	 ("C-x C-f" . counsel-find-file)
-	 ("C-c j" . counsel-git-grep)))
+	 ("C-c j" . counsel-git-grep)
+         ("C-c a" . counsel-ag)
+         ("C-`" . counsel-mark-ring)))
 
 ;; imenu-list takes your imenu out of the minibuffer
 (use-package imenu-list
@@ -142,8 +149,16 @@
   :ensure
   :init)
 
-
-
+;; ace-window is a better window switcher
 (use-package ace-window
   :ensure
   :bind ("M-o" . ace-window))
+
+;; View
+(use-package view
+  :bind (("C-v" . 'View-scroll-half-page-forward)
+         ("M-v" . 'View-scroll-half-page-backward)))
+
+;; Key bindings
+(global-set-key (kbd "C-x /") 'comment-or-uncomment-region)
+(setq-default smerge-command-prefix "\C-cv")
